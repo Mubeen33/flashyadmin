036f5e8467fbfeec0f4189cd0be26cbc4cc14312
Mubeen <mubeen33@gmail.com>
2020-08-05 17:46:14 +0500

    Mubeen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,107 +1,161 @@
 <!DOCTYPE html>
-<html lang="en">
-    <!-- START: Head-->
-    <head>
-        <meta charset="UTF-8">
-        <title>Flashy Admin</title>
-        <link rel="shortcut icon" href="dist/images/favicon.ico" />
-        <meta name="viewport" content="width=device-width,initial-scale=1"> 
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
 
-        <!-- START: Template CSS-->
-        <link rel="stylesheet" href="{{ asset('dist/vendors/bootstrap/css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{ asset('dist/vendors/jquery-ui/jquery-ui.min.css')}}">
-        <link rel="stylesheet" href="{{ asset('dist/vendors/jquery-ui/jquery-ui.theme.min.css')}}">
-        <link rel="stylesheet" href="{{ asset('dist/vendors/simple-line-icons/css/simple-line-icons.css')}}">        
-        <link rel="stylesheet" href="{{ asset('dist/vendors/flags-icon/css/flag-icon.min.css')}}"> 
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>Flashybuy|Admin</title>
+    <link rel="apple-touch-icon" href="{{ asset('app-assets/images/ico/apple-icon-120.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets/images/ico/favicon.ico')}}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
-        <!-- END Template CSS-->     
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors.min.css')}}">
+    <!-- END: Vendor CSS-->
 
-        <!-- START: Page CSS-->   
-        <link rel="stylesheet" href="{{ asset('dist/vendors/social-button/bootstrap-social.css')}}"/>   
-        <!-- END: Page CSS-->
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/components.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/semi-dark-layout.css')}}">
 
-        <!-- START: Custom CSS-->
-        <link rel="stylesheet" href="{{ asset('dist/css/main.css')}}">
-        <!-- END: Custom CSS-->
-    </head>
-    <!-- END Head-->
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/vertical-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/colors/palette-gradient.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/pages/authentication.css')}}">
+    <!-- END: Page CSS-->
 
-    <!-- START: Body-->
-    <body id="main-container" class="default">
-        <!-- START: Main Content-->
-        <div class="container">
-            <div class="row vh-100 justify-content-between align-items-center">
-                <div class="col-12">
-                    <form method="POST" action="{{ route('login') }}" class="row row-eq-height lockscreen  mt-5 mb-5">
-                        @csrf
-                        <div class="lock-image col-12 col-sm-5"></div>
-                        <div class="login-form col-12 col-sm-7">
-                            <div class="form-group mb-3">
-                                <label for="emailaddress">Email address</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}}">
+    <!-- END: Custom CSS-->
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+</head>
+<!-- END: Head-->
 
-                            <div class="form-group mb-3">
-                                <label for="password">Password</label>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<!-- BEGIN: Body-->
 
-                            <div class="form-group mb-3">
-                                <div class="custom-control custom-checkbox">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                    <label class="custom-control-label" for="checkbox-signin">Remember me</label>
+<body class="vertical-layout vertical-menu-modern 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page" data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <section class="row flexbox-container">
+                    <div class="col-xl-8 col-11 d-flex justify-content-center">
+                        <div class="card bg-authentication rounded-0 mb-0">
+                            <div class="row m-0">
+                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                                    <img src="{{ asset('app-assets/images/pages/login.png')}}" alt="branding logo">
+                                </div>
+                                <div class="col-lg-6 col-12 p-0">
+                                    <div class="card rounded-0 mb-0 px-2">
+                                        <div class="card-header pb-1">
+                                            <div class="card-title">
+                                                <h4 class="mb-0">Login</h4>
+                                            </div>
+                                        </div>
+                                        <p class="px-2">Welcome back, please login to your account.</p>
+                                        <div class="card-content">
+                                            <div class="card-body pt-1">
+                                                <form  method="POST" action="{{ route('login') }}">
+                                                    @csrf
+                                                    <fieldset class="form-label-group form-group position-relative has-icon-left">
+                                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-user"></i>
+                                                        </div>
+                                                        @error('email')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                        <label for="user-name">Email</label>
+                                                    </fieldset>
+
+                                                    <fieldset class="form-label-group position-relative has-icon-left">
+                                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-lock"></i>
+                                                        </div>
+                                                        @error('password')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                        <label for="user-password">Password</label>
+                                                    </fieldset>
+                                                    <div class="form-group d-flex justify-content-between align-items-center">
+                                                        <div class="text-left">
+                                                            <fieldset class="checkbox">
+                                                                <div class="vs-checkbox-con vs-checkbox-primary">
+                                                                    <input type="checkbox">
+                                                                    <span class="vs-checkbox">
+                                                                        <span class="vs-checkbox--check">
+                                                                            <i class="vs-icon feather icon-check"></i>
+                                                                        </span>
+                                                                    </span>
+                                                                    <span class="">Remember me</span>
+                                                                </div>
+                                                            </fieldset>
+                                                        </div>
+                                                        <!-- <div class="text-right"><a href="auth-forgot-password.html" class="card-link">Forgot Password?</a></div> -->
+                                                    </div>
+                                                    <!-- <a href="auth-register.html" class="btn btn-outline-primary float-left btn-inline">Register</a> -->
+                                                    <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="login-footer">
+                                            <div class="divider">
+                                                <div class="divider-text"></div>
+                                            </div>
+                                            <!-- <div class="footer-btn d-inline">
+                                                <a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>
+                                                <a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>
+                                                <a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>
+                                                <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a>
+                                            </div> -->
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div class="form-group mb-0">
-                                <button class="btn btn-primary" type="submit"> Log In </button>
-                            </div>
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-                            <!-- <p class="my-2 text-muted">--- Or connect with ---</p>
-                            <a class="btn btn-social btn-dropbox text-white mb-2">
-                                <i class="icon-social-dropbox align-middle"></i>
-                            </a>
-                            <a class="btn btn-social btn-facebook text-white mb-2">
-                                <i class="icon-social-facebook align-middle"></i>
-                            </a>                                   
-                            <a class="btn btn-social btn-github text-white mb-2">
-                                <i class="icon-social-github align-middle"></i>
-                            </a>
-                            <a class="btn btn-social btn-google text-white mb-2">
-                                <i class="icon-social-google align-middle"></i>
-                            </a>
-                            <div class="mt-2">Don't have an account? <a href="page-register.html">Create an Account</a></div> -->
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </section>
 
             </div>
         </div>
-        <!-- END: Content-->
+    </div>
+    <!-- END: Content-->
 
-        <!-- START: Template JS-->
-        <script src="{{ asset('dist/vendors/jquery/jquery-3.3.1.min.js')}}"></script>
-        <script src="{{ asset('dist/vendors/jquery-ui/jquery-ui.min.js')}}"></script>
-        <script src="{{ asset('dist/vendors/moment/moment.js')}}"></script>
-        <script src="{{ asset('dist/vendors/bootstrap/js/bootstrap.bundle.min.js')}}"></script>    
-        <script src="{{ asset('dist/vendors/slimscroll/jquery.slimscroll.min.js')}}"></script>
 
-        <!-- END: Template JS-->  
-    </body>
-    <!-- END: Body-->
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{ asset('app-assets/vendors/js/vendors.min.js')}}"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="{{ asset('app-assets/js/core/app-menu.js')}}"></script>
+    <script src="{{ asset('app-assets/js/core/app.js')}}"></script>
+    <script src="{{ asset('app-assets/js/scripts/components.js')}}"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <!-- END: Page JS-->
+
+</body>
+<!-- END: Body-->
+
 </html>
