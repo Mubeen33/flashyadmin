2877adcb2f3f437a7e1ced84fa559881e4bf2472
kashif02018 <38316560+kashif02018@users.noreply.github.com>
2020-08-10 11:34:06 +0500

    null

---
+++ app/Http/Controllers/CustomFieldController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CustomField;
+use App\Category;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class CustomFieldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+		$categories = DB::table('categories')
+					->select('*')
+					->orderBy('id', 'DESC')
+					->where('parent_id', 0)
+					->get();
+        return view('custom-fields', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('add-category');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+//        $request->validate([
+//            'name' => 'required',
+//            'slug' => 'required',
+//        ]);
+//  
+//        Category::create($request->all());
+//        return redirect()->route('categories.index')->with('success','Category created successfully.');        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //return view('edit-category',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+//        $request->validate([
+//            'name' => 'required',
+//            'slug' => 'required',
+//        ]);
+//  
+//        $category->update($request->all());
+//        return redirect()->route('categories.index')->with('success','Category updated successfully.');        
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
