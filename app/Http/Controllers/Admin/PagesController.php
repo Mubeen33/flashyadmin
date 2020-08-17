8976e9613ae920b7090996b280e9f4ba133ea06e
RI Rijan <38884716+DeveloperRijan@users.noreply.github.com>
2020-08-17 16:24:42 +0600

    Rijan

---
+++ app/Http/Controllers/Admin/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    public function get_page($pageName){
+    	if(view()->exists('AdminViews.Pages.' . $pageName)){
+            return view("AdminViews.Pages.$pageName");
+        }else{
+            return abort(404);
+        }
+    }
+}
