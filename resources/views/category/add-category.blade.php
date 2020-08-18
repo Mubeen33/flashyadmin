3daf4aa2910fc1896cd0358b8d161782b1ecb71a
Mian Dawood <maindawood31@gmail.com>
2020-08-18 17:33:52 +0500

    Category add, update, active ,disable Done without Parent_id concept

--- resources/views/category/add-category.blade.php
+++ resources/views/category/add-category.blade.php
@@ -112,7 +112,9 @@
                                                                       @foreach ($categories as $item)
                                                                   <option value="{{$item->id}}">{{$item->name}}</option> 
                                                                       @endforeach
-                                                                                                                             
+                                                                     
+                                                                          
+                                
                                                                   </select>
                                                                 </div>
                                                             </div>
--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,12 @@ Route::post('update-category','category\CategoryController@updatecategory');
 Route::get('category-active/{id}','category\CategoryController@activecategory');
 Route::get('category-disable/{id}','category\CategoryController@disableAcategory');
 
+Route::get('/childs', function () {
+	return('all is well');
+	$main_id=input::get('par_id');
+	$childs=Categories:: where('parent_id','=',$main_id);
+  return Response::json($childs);
+});
 // End Categories
 Route::group(['as'=>'admin.', 'namespace'=>'Admin', 'middleware' => ['auth']], function(){
 	//vendors controller
