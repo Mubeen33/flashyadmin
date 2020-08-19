3a02e71213b24c6d4afa90e91a13c9143c4fc51e
Mubeen <mubeen33@gmail.com>
2020-08-19 15:28:54 +0500

    Mubeen

--- app/Http/Controllers/customfields/CustomfieldController.php
+++ app/Http/Controllers/customfields/CustomfieldController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\customfields;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Cutomfield;
+use App\Category;
 
 class CustomfieldController extends Controller
 {
