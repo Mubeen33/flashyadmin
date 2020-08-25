12ba769bff116899c90aa617f531bf56a8fe81ec
Mubeen <mubeen33@gmail.com>
2020-08-25 10:20:42 +0500

    Mubeen

--- resources/views/customfields/add-customfields.blade.php
+++ resources/views/customfields/add-customfields.blade.php
@@ -30,7 +30,7 @@
                                                                     <span>Parent Category</span>
                                                                 </div>
                                                                 <div class="col-md-8">
-                                                                    <select class="form-control" name="parent_id[]" onchange="get_subcategories(this.value, 0);" required>
+                                                                    <select class="form-control" name="parent_id[]" onchange="get_subcategories(this.value, 0);">
                                                                         <option value="">none</option>
                                                                         @foreach($parentCategory as $category)
                                                                             <option value="{{$category->id}}">{{$category->name}}</option>
