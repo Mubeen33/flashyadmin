2ce88e27027dc715de8d5c5b86ef3e16fddb9e0d
Mubeen33 <mubeenbaig33@gmail.com>
2020-09-20 12:53:36 +0500

    Revert "Merge branch 'master' of https://github.com/Mubeen33/flashyadmin"
    This reverts commit 60284c7c700f11f29d932a72f8772d2ffa12095d, reversing
    changes made to 48b22a3ca2b5b894380a183a9247984eb3aba641.

--- resources/views/product/partials/product-list.blade.php
+++ resources/views/product/partials/product-list.blade.php
@@ -2,6 +2,58 @@
 
 @if(intval($content->get_vendor->active) === 1 && intval($content->approved) === 1)
 
+
+@if($get_vendor->active == 1)
+<tr>
+    <th scope="row">{{ $key+1 }}</th>
+    <td>
+        @if($get_vendor)
+        {{ $get_vendor->first_name }} {{ $get_vendor->last_name }}
+        @endif
+    </td>                                          
+    <td>{{ $content->title }}</td>
+    <td>
+        @if($get_cat)
+        {{ $get_cat->name }}
+        @endif
+    </td>
+    <td>
+        @if($get_image)
+        <img src="{{ $get_image->image }}" width="80px" height="50px">
+        @endif
+    </td>
+    <td>{{ $content->made_by }}</td>
+    <td>{{ $content->product_type }}</td>
+    <td>
+          {{ $content->created_at->format('d/m/Y') }}
+    </td>
+    <td>
+        @if(intval($content->approved) === 0 && intval($content->rejected) === 0 && intval($content->disable) === 0)
+            <span class="badge badge-danger">Pending</span>
+        @elseif(intval($content->approved) === 1 && intval($content->rejected) === 0 && intval($content->disable) === 0)
+            <span class="badge badge-success">Approved</span>
+        @elseif(intval($content->rejected) === 1 && intval($content->approved) === 0 && intval($content->disable) === 0)
+            <span class="badge badge-warning">Rejected</span>
+        @elseif(intval($content->disable) === 1 && intval($content->approved) === 0 && intval($content->rejected) === 0)
+            <span class="badge badge-danger">Disabled</span>
+        @endif
+    </td>
+    <td>
+        <div class="btn-group">
+            <div class="dropdown">
+                <button class="btn btn-dark btn-sm dropdown-toggle mr-1" type="button" id="dropdownMenuButton7" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Actions
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton7">
+                    <a class="dropdown-item" href="{{ route('admin.productDetails.get', encrypt($content->id)) }}">Show</a>
+                    
+                    @if(intval($content->approved) === 1 && intval($content->rejected) === 0 && intval($content->disable) === 0)
+                        <a  class="dropdown-item" href="{{route('admin.productEdit.post', encrypt($content->id))}}">Edit</a>
+                    @else
+                        <a  class="dropdown-item" href="{{route('admin.productControl.post', encrypt($content->id))}}">Approve</a>
+                    @endif    
+                    <a onclick="return confirm('Are you sure to disable?')" class="dropdown-item" href="{{ route('admin.disableProduct.post', encrypt($content->id)) }}">Disable</a>
+
     @if(!$content->get_product_variations->isEmpty())
         
         @foreach($content->get_product_variations as $v_key=>$variation)
@@ -109,6 +161,7 @@
                         <a  class="dropdown-item" href="{{route('admin.productVendor.get', encrypt($content->id))}}">Product Vendor</a>
                         <a onclick="return confirm('Are you sure to disable?')" class="dropdown-item" href="{{ route('admin.disableProduct.post', encrypt($content->id)) }}">Disable</a>
                     </div>
+
                 </div>
             </div>
             
@@ -122,7 +175,7 @@
 @endif
 
 
-
+@endif
 @endforeach
 <tr>
     <td colspan="9">{!! $data->links() !!}</td>
